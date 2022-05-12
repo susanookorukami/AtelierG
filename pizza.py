0df12d88197040c39f9204652d2d6eddde0cc2a2
Yassamine.ag <77329160+l3miage-agouramy@users.noreply.github.com>
2022-05-12 16:52:47 +0200

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,1 +1,13 @@
-print("Hello World from PIZZA")
+class Pizza:
+  
+  def __init__(self, name, base, ingredients):
+    self.name = name
+    self.base = base
+    self.ingredients = ingredients
+    self.state = False
+    
+  def create_pizza(self):
+    pass
+  
+  def is_done(self):
+    pass
